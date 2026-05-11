c780cfaccd2a4e0fbb7e41404e7536f0ed6199c7
Thai Nguyen Hoang <jaskai163@gmail.com>
2026-05-11 11:55:40 +0700

    Base

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -2,10 +2,3 @@ const express = require('express');
 const router = express.Router();
 const roomController = require('../controllers/roomController');
 
-// GET /api/rooms
-router.get('/', roomController.getAllRooms);
-
-// POST /api/rooms/search
-router.post('/search', roomController.searchRooms);
-
-module.exports = router;
